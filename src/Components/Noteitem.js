50d29ef7e053c031ec9019aef6ab0e0370eaaf44
Soumya Sachin Panda <pandasoumyasachin@gmail.com>
2023-08-23 12:51:39 +0530

    syntax updated

--- src/Components/Noteitem.js
+++ src/Components/Noteitem.js
@@ -13,7 +13,7 @@ const Noteitem = (props) => {
             <div className="card my-3 border-primary bg-body-tertiary shadow">
 
                 <div className="card-body">
-                    <h5 className="card-title text-primary">{note.title}</h5>
+                    <h5 className="card-title text-primary">{note.title }</h5>
                     <p className="card-text">{note.description}  </p>
                     <i className="far fa-trash-alt mx-2" onClick = {()=>{deleteNote(note._id); props.showAlert("Deleted Successfully" , "success")}}></i>
                     <i className="far fa-edit mx-2" onClick = {()=>{updateNote(note)}}></i>
